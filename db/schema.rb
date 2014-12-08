ff2145c95da99c4a4b68f7b03222c2ad96fdd6f5
Chris <clwillingham@gmail.com>
2014-12-07 21:57:26 -0500

    fixing post body data type and limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028144908) do
+ActiveRecord::Schema.define(version: 20141208025401) do
 
   create_table "pages", force: true do |t|
     t.string   "title"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20141028144908) do
   create_table "posts", force: true do |t|
     t.integer  "user_id"
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
