6d092e973f5e032cb1f99b813d4d9cff7991e06e
Chris <clwillingham@gmail.com>
2016-05-25 11:53:10 -0400

    adding carrierwave config to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  CarrierWave.configure do |config|
+    config.dropbox_app_key = ENV["APP_KEY"]
+    config.dropbox_app_secret = ENV["APP_SECRET"]
+    config.dropbox_access_token = ENV["ACCESS_TOKEN"]
+    config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    config.dropbox_user_id = ENV["USER_ID"]
+    config.dropbox_access_type = "app_folder"
+  end
 end
